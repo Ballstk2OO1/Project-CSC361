944ebe5244e87af450b81f690fac5f20d3d8c692
Ballstk_ <thiti45909_wasd@hotmail.com>
2021-11-10 12:23:25 +0700

    login validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,8 @@
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+const { Router } = require("express");
 const User = require("../models/user_schema");
-const { registerValidation } = require("../validation")
+const { registerValidation, loginValidation } = require("../validation")
 // const jwt = require("../jwt");
-
-exports.login = async (req, res) => {
-
-};
 
 exports.register = async (req, res) => {
 
@@ -21,18 +18,45 @@ exports.register = async (req, res) => {
         return res.status(400).send("Email already exist.");
     }
 
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     // Create new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
     });
 
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({ user: user._id });
     } catch (err) {
         res.status(400).send(err);
     }
 
+};
+
+exports.login = async (req, res) => {
+
+    // Validate infomation
+    const { error } = loginValidation(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    // Checking if the email exist
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(400).send("Email doesn't exist.");
+    }
+
+    // Password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) {
+        return res.status(400).send("Email and Password doesn't match.");
+    }
+
+    res.send("Login Success")
 };
